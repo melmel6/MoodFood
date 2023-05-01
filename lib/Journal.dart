151b272c5fd0e5ee92159fbbc1f77f635cd689b6
nikarageorgos <karageorgosni@gmail.com>
2023-05-01 19:28:33 +0200

    journal

--- lib/Journal.dart
+++ lib/Journal.dart
@@ -39,7 +39,73 @@ class _JournalPageState extends State<JournalPage> {
       'Yes, I had a really difficult project at work that I had to finish by the end of the day, and it was causing me anxiety.';
 
 
-   @override
+  List<Widget> _buildBoxes() {
+    List<Widget> boxes = [];
+    
+     boxes.add(_buildInfoContainer(
+        icon: Icons.book,
+        title: 'Understanding Emotional Eating',
+        info: 'Description 1',
+        icon2: Icons.info,
+        info2: 'Description 2',
+        icon3: Icons.info,
+        info3: 'Description 3',
+      ));
+     boxes.add(_buildInfoContainer(
+        icon: Icons.info,
+        title: 'Identifying Emotional Triggers',
+        info: 'Description 1',
+        icon2: Icons.info,
+        info2: 'Description 2',
+        icon3: Icons.info,
+        info3: 'Description 3',
+      ));
+       boxes.add(_buildInfoContainer(
+        icon: Icons.info,
+        title: 'Coping Strategies and Techniques',
+        info: 'Description 1',
+        icon2: Icons.info,
+        info2: 'Description 2',
+        icon3: Icons.info,
+        info3: 'Description 3',
+      ));
+       boxes.add(_buildInfoContainer(
+        icon: Icons.info,
+        title: 'Nutrition and Healthy Eating',
+        info: 'Description 1',
+        icon2: Icons.info,
+        info2: 'Description 2',
+        icon3: Icons.info,
+        info3: 'Description 3',
+      ));
+       boxes.add(_buildInfoContainer(
+        icon: Icons.book,
+        title: 'Success Stories and Inspirational Content',
+        info: 'Description 1',
+        icon2: Icons.info,
+        info2: 'Description 2',
+        icon3: Icons.info,
+        info3: 'Description 3',
+      ));
+    return boxes;
+  }
+
+  
+Widget _buildGridView() {
+  return ListView.builder(
+    scrollDirection: Axis.horizontal,
+    padding: EdgeInsets.all(8),
+    itemCount: _buildBoxes().length,
+    itemBuilder: (BuildContext context, int index) {
+      return Container(
+        width: MediaQuery.of(context).size.width * 0.5, // Adjust the width as needed
+        child: _buildBoxes()[index],
+      );
+    },
+  );
+}
+
+@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -52,101 +118,168 @@ class _JournalPageState extends State<JournalPage> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(child: _buildGridView()),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(Icons.calendar_today),
-                SizedBox(width: 8),
                 Text(
-                  DateFormat('MMMM d, y h:mm a').format(DateTime.now()),
+                  'Reflect on your emotions and eating habits with this prompt:',
                   style: TextStyle(
                     fontSize: 16,
                     fontWeight: FontWeight.bold,
                     fontFamily: 'Montserrat',
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 32),
-            Text(
-              'Reflect on your emotions and eating habits with this prompt:',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Montserrat',
-              ),
-            ),
-            SizedBox(height: 16),
-            _buildQuestionBox(
-              '1. How hungry are you on a scale from 1-10?',
-              DropdownButtonFormField<int>(
-                value: _selectedValue,
-                decoration: InputDecoration(
-                  hintText: 'Pick a value',
-                  contentPadding: EdgeInsets.all(16),
-                  border: OutlineInputBorder(),
+                SizedBox(height: 16),
+                Container(
+  alignment: Alignment.centerRight,
+  child:
+                Row(
+                  mainAxisSize: MainAxisSize.min, 
+                  children: [
+                    Icon(Icons.calendar_today),
+                    SizedBox(width: 8),
+                    Text(
+                      DateFormat('MMMM d, y h:mm a').format(DateTime.now()),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ],
                 ),
-                items: List.generate(
-                  10,
-                  (index) => DropdownMenuItem(
-                    value: index + 1,
-                    child: Text('${index + 1}'),
+                ),
+                SizedBox(height: 32),
+                _buildQuestionBox(
+                  '1. How hungry are you on a scale from 1-10?',
+                  DropdownButtonFormField<int>(
+                    value: _selectedValue,
+                    decoration: InputDecoration(
+                      hintText: 'Pick a value',
+                      contentPadding: EdgeInsets.all(16),
+                      border: OutlineInputBorder(),
+                    ),
+                    items: List.generate(
+                      10,
+                      (index) => DropdownMenuItem(
+                        value: index + 1,
+                        child: Text('${index + 1}'),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                        _controller1.text = _selectedValue!.toString();
+                      });
+                    },
                   ),
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedValue = value;
-                    _controller1.text = _selectedValue!.toString();
-                  });
-                },
-              ),
+                SizedBox(height: 16),
+                _buildQuestionBox(
+                  '2. What emotions are you experiencing right now?',
+                  TextField(
+                    controller: _controller2,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(16),
+                      hintText: 'Answer here...',
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                _buildQuestionBox(
+                  '3. Did you experience any stressful or triggering events today?',
+                  TextField(
+                    controller: _controller3,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(16),
+                      hintText: 'Answer here...',
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 32),
+                _buildLastEntryHeader(),
+                SizedBox(height: 4),
+                _buildLastEntryBox(),
+              ],
             ),
-            SizedBox(height: 16),
-            _buildQuestionBox(
-              '2. What emotions are you experiencing right now?',
-              TextField(
-                controller: _controller2,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(16),
-                  hintText: 'Answer here...',
-                  border: InputBorder.none,
-                ),
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            _buildQuestionBox(
-              '3. Did you experience any stressful or triggering events today?',
-              TextField(
-                controller: _controller3,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(16),
-                  hintText: 'Answer here...',
-                  border: InputBorder.none,
-                ),
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-            ),
-            SizedBox(height: 32),
-            _buildLastEntryHeader(),
-            SizedBox(height: 32),
-            _buildLastEntryBox(),
-          ],
-        ),
+          ),
+          ),
+        ],
       ),
     );
   }
+  
+
+  
+Widget _buildInfoContainer({
+  required IconData icon,
+  required String title,
+  required String info,
+  required IconData icon2,
+  required String info2,
+  required IconData icon3,
+  required String info3,
+}) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 4),
+    padding: EdgeInsets.all(4),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          blurRadius: 4,
+          offset: Offset(0, 2),
+        ),
+      ],
+      color: Colors.white,
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 8),
+        ExpansionTile(
+          title: Text('Learn More'),
+          children: [
+            ListTile(
+              leading: Icon(icon),
+              title: Text(info),
+            ),
+            ListTile(
+              leading: Icon(icon2),
+              title: Text(info2),
+            ),
+            ListTile(
+              leading: Icon(icon3),
+              title: Text(info3),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+} 
 
 
     Widget _buildQuestionBox(String question, Widget inputWidget) {
@@ -174,7 +307,7 @@ class _JournalPageState extends State<JournalPage> {
   }
 
 
-   Widget _buildLastEntryHeader() {
+    Widget _buildLastEntryHeader() {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       decoration: BoxDecoration(
@@ -192,7 +325,7 @@ class _JournalPageState extends State<JournalPage> {
               fontFamily: 'Montserrat',
             ),
           ),
-          TextButton(
+          OutlinedButton(
             onPressed: () {
               // Navigate to the view all entries page
             },
@@ -202,14 +335,19 @@ class _JournalPageState extends State<JournalPage> {
                 fontSize: 16,
                 fontFamily: 'Montserrat',
                 fontWeight: FontWeight.normal,
-                color: Theme.of(context).primaryColor,
+                color: Colors.black,
               ),
+            ),
+            style: OutlinedButton.styleFrom(
+              primary: Colors.black,
             ),
           ),
         ],
       ),
     );
   }
+
+
     Widget _buildLastEntryBox() {
     return Container(
       decoration: BoxDecoration(
@@ -286,4 +424,4 @@ class _JournalPageState extends State<JournalPage> {
     );
   }
 
-} 
+}
